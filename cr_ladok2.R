51e3a08d9b30c7769ec8e55f1614f8ee23c41239
bjornerstedt <jonas@bjornerstedt.org>
2017-11-15 12:02:37 +0100

    Djurönäs results

--- cr_ladok2.R
+++ cr_ladok2.R
@@ -13,10 +13,21 @@ prov =
     select(kursid = kurs, prov, delkurs=benamne) %>% 
     inner_join(kurs, by = "kursid") 
 
+undkprov = 
+    undkprov %>% 
+    rename(kursid=kurs) %>% 
+    group_by( pnr, kursid, prov) %>% 
+    summarise(u = n(), idatum = last(idatum), betyg = last(betyg))
+    
+underkänd = undkprov %>% 
+    group_by(pnr, kursid) %>% 
+    summarise(u = sum(u))
+    
 ffgkurs = 
     ffgkurs %>% 
     select(pnr, kursid = kurs, progr, termin, kurstakt, ort, omgang, regdatum=idatum) %>% 
     inner_join(kurs, by = "kursid") %>% 
+    left_join(underkänd, by = c("kursid", "pnr") ) %>% 
     collect() %>% 
     mutate(
         ar = trunc(termin/10),
@@ -26,21 +37,28 @@ ffgkurs =
         pnr_chr = is.na(as.numeric(str_sub( pnr , 7,7))), 
         kvinna = as.numeric(str_sub( pnr , 9, 9)) %% 2 == 0,
         birthyear = as.numeric(str_sub( pnr , 1, 2)),
-        alder = 2017 - birthyear - if_else(birthyear < 20, 2000, 1900)
+        alder = 2017 - birthyear - if_else(birthyear < 20, 2000, 1900),
+        u = coalesce(u, 0L)
     ) %>% 
+    rename(utot = u) %>% 
     select(-termin, -kurstakt, -termin, -ort, -omgang, -birthyear)
 
+godkprov = godkprov %>% 
+    select(kursid=kurs, pnr,  prov, betyg, idatum) 
+    
 godkprov = 
     godkprov %>% 
-    select(kursid=kurs, pnr,  prov, betyg, idatum) %>% 
+    left_join(undkprov %>% select(-betyg, -idatum) , by = c("pnr", "kursid", "prov")) %>% 
+    bind_rows(undkprov %>% anti_join(godkprov, by = c("pnr", "kursid", "prov") )) %>% 
+    mutate(u = coalesce(u, 0L)) %>% 
     inner_join(prov, by = c("kursid", "prov")) %>% 
-    collect() %>% 
     mutate(
         idatum = as_date(idatum)
     ) %>% 
     left_join(ffgkurs, by = c("kursid", "kursnamn","pnr") ) %>% 
     mutate(
-        duration = as.integer(idatum - regdatum)
+        duration = as.integer(idatum - regdatum),
+        kursnamn = if_else( is.na(str_match(kursnamn, "^Economics .$")), "Masters", kursnamn )
     )
 
 godkkurs = 
@@ -50,6 +68,9 @@ godkkurs =
     mutate(
         datum = as_date(datum)
     )
+
+# Testa att alla godkända var registrerade
+if(anti_join(godkkurs, ffgkurs, by = c("pnr", "kursid")) %>% tally > 0) stop("Godkända som inte registrerats")
 
 kursresult = 
     left_join(ffgkurs, godkkurs, by = c("pnr", "kursid")) %>% 
@@ -62,7 +83,19 @@ kursresult =
         kursnamn = if_else( is.na(str_match(kursnamn, "^Economics .$")), "Masters", kursnamn )
     ) 
 
+# Beräkna godkända per delkurs
+andelar_prov = godkprov %>% 
+    group_by(kursid, prov, betyg) %>% 
+    summarise(antal = n(), u = sum(u)) %>% 
+    group_by(kursid, prov) %>% 
+    mutate(
+        andel = antal / sum(antal) * 100,
+        betyg = factor(betyg, levels = c("U","VG","G"))
+    ) 
+
+# Beräkna godkända per kurs
 andelar = kursresult %>% 
+    filter(betyg!="U" | utot!=0) %>%  # Ta bort de som inte tenterar alls
     group_by(kursnamn, termindatum, betyg) %>% 
     summarise(antal = n()) %>% 
     group_by(kursnamn, termindatum) %>% 
@@ -73,6 +106,7 @@ andelar = kursresult %>%
     filter( year(termindatum) < 2017)
 
 andelar_program = kursresult %>% 
+    filter(betyg!="U" | utot!=0) %>%  # Ta bort de som inte tenterar alls
     mutate(
         program = !is.na(progr)
     ) %>% 
@@ -85,6 +119,6 @@ andelar_program = kursresult %>%
     ) %>% 
     filter( akademiskt_ar < 2017)
 
-save(ffgkurs, godkprov, kursresult, andelar, andelar_program, 
-    file = "~/Library/Mobile Documents/com~apple~CloudDocs/Work/data/admin/ladok2.Rdata")
+save(ffgkurs, godkprov, kursresult, andelar, andelar_program, andelar_prov,
+    file = "ladok2.Rdata")
 
--- cr_reservation.R
+++ cr_reservation.R
@@ -16,4 +16,42 @@ reserve_nek <- bind_rows(
 ) %>% 
     filter(!is.na(Vecka))
 
-saveRDS(reserve_nek, file = "~/Library/Mobile Documents/com~apple~CloudDocs/Work/data/admin/reervations.rds")
+saveRDS(reserve_nek, file = "~/Library/Mobile Documents/com~apple~CloudDocs/Work/data/admin/reservations.rds")
+
+# Set libPaths.
+.libPaths("/Users/jonasbjornerstedt/.exploratory/R/3.4")
+
+# Load required packages.
+library(janitor)
+library(lubridate)
+library(hms)
+library(tidyr)
+library(stringr)
+library(readr)
+library(forcats)
+library(RcppRoll)
+library(dplyr)
+library(tibble)
+library(exploratory)
+
+# Steps to produce reservera_A
+`reservera_A` <- exploratory::select_columns(exploratory::clean_data_frame(exploratory::read_excel_file( "/Users/jonasbjornerstedt/Library/Mobile Documents/com~apple~CloudDocs/Work/data/admin/TimeEdit_1001NE_Nationalekonomi_A_2017-10-12_19_11.xls", sheet = "TimeEdit", na="NA", skip=5, col_names=TRUE, trim_ws=FALSE)),"Vecka","Veckodag","Startdatum","Starttid","Slutdatum","Sluttid","Lokal","Undervisningstyp","Kurs","Delkurs","Person") %>% exploratory::clean_data_frame()
+
+# Steps to produce the output
+exploratory::select_columns(exploratory::clean_data_frame(exploratory::read_excel_file( "/Users/jonasbjornerstedt/Library/Mobile Documents/com~apple~CloudDocs/Work/data/admin/TimeEdit_1073NE_Avancerad_nationalekonomi_1077NE_Avancerad_nationalekonomi__18_Kurs_20_2017-10-12_19_08.xls", sheet = "TimeEdit", na="NA", skip=5, col_names=TRUE, trim_ws=TRUE)),"Vecka","Veckodag","Startdatum","Starttid","Slutdatum","Sluttid","Lokal","Undervisningstyp","Kurs","Delkurs","Person","Anmärkning") %>% exploratory::clean_data_frame() %>%
+    
+    # Add A course
+    bind_rows(reservera_A) %>%
+    filter(!is.na(Vecka)) %>%
+    mutate(Startdatum = ymd(Startdatum), Slutdatum = ymd(Slutdatum), Starttid = parse_time(Starttid), Sluttid = parse_time(Sluttid), Timmar = (Sluttid - Starttid) / 3600) %>%
+    separate(Vecka, into = c("Vecka", "Veckonr"), sep = "\\s+", remove = TRUE, convert = TRUE) %>%
+    mutate(Period = if_else(Veckonr < 25,1,2), `År` = year(Startdatum)) %>%
+    select(-Slutdatum, -Vecka, -Anmärkning, -Veckonr) %>%
+    separate(Kurs, into = c("Kurs"), sep = "\\s*\\,\\s*", remove = TRUE, convert = TRUE, extra = "drop") %>%
+    separate(Delkurs, into = c("Delkurs"), sep = "\\s*\\,\\s*", remove = TRUE, extra = "drop") %>%
+    mutate(Delkurs = str_replace(Delkurs, "Valbar kurs: ", "")) %>%
+    mutate(Timmar = as.numeric(Timmar)) %>%
+    group_by(År, Period, Person, Delkurs) %>%
+    summarize(Timmar = sum(Timmar, na.rm = TRUE)) %>%
+    arrange(År, Period, Delkurs, Person)
+
